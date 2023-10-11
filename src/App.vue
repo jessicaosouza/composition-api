67a44d7ac62aa389b466d6a9528c95f9eb5f4e0b
Jessica Souza <jessicathedoc@gmail.com>
2023-10-11 19:52:31 -0300

    class4: vue composition api setup method

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<h1>
+		{{ name }}
+	</h1>
+	<button @click="placeOrder">Place Order</button>
 </template>
+
+<script>
+	export default {
+		setup(){
+			const name = "The Snazzy Burger"
+
+			const placeOrder = () => { alert("You're order has been placed!") }
+			return { name, placeOrder }
+		},
+
+		
+	}
+</script>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
