cb9ce512f28e16dd76aea711777b9e16fbe48e66
Jessica Souza <jessicathedoc@gmail.com>
2023-10-14 12:06:37 -0300

    class17: using the usepost composable in multiple components

--- src/composables/usePost.js
+++ src/composables/usePost.js
@@ -2,12 +2,20 @@ import { ref } from "vue"
 
 export default function usePost(){
     const posts = ref([])
+    const post = ref(null)
+    const baseURL = "https://jsonplaceholder.typicode.com/posts"
     const fetchAll = async () => {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const response = await fetch(baseURL)
         posts.value = await response.json()
+    }
+    const fetchOne = async (id) => {
+        const response = await fetch(`${baseURL}/${id}`)
+        post.value = await response.json()
     }
 
     return {
+        post,
+        fetchOne,
         posts,
         fetchAll
     }
